4de6ee4278d81a8286c6735840334dd8abd51f9d
kawamonR <92916593+kawamonR@users.noreply.github.com>
2023-09-29 23:55:56 +0900

    Update main.cpp
    魔法陣の描画についての説明を追加しました。

--- mac_drawpad_2021_v1/DrawPad/main.cpp
+++ mac_drawpad_2021_v1/DrawPad/main.cpp
@@ -3,36 +3,36 @@
 
 void DrawMain()
 {
-    Clear(kColorBlack);
+    Clear(kColorBlack);//画面を黒くする
 
     DrawLine(-320, 0, 319, 0, kColorGray);
     DrawLine(0, -240, 0, 239, kColorGray);
 
     for (int i = 0; i < 5; i++) {
-        FillCircle(i*100-200, -190, 30, kColorBlue, M_PI/6+i*M_PI/2, -M_PI/6+i*M_PI/2);
+        FillCircle(i*100-200, -190, 30, kColorBlue, M_PI/6+i*M_PI/2, -M_PI/6+i*M_PI/2);//円をを描く
     }
 
     for (int x = -100; x <= 100; x+=10) {
-        DrawCircle(x, 0, 150, kColorBlue);
+        DrawCircle(x, 0, 150, kColorBlue);//円をを描く
     }
 
     for (int x = -250; x < -50; x+=10) {
-        Paint(x+5, 0, kColorDarkBlue, kColorBlue);
+        Paint(x+5, 0, kColorDarkBlue, kColorBlue);//円をを描く
     }
     for (int x = 50; x < 250; x+=10) {
-        Paint(x+5, 0, kColorDarkBlue, kColorBlue);
+        Paint(x+5, 0, kColorDarkBlue, kColorBlue);//円をを描く
     }
 
-    DrawText("Hello, world!!", -12*14/2, 44, kColorWhite);
+    DrawText("Hello, world!!", -12*14/2, 44, kColorWhite);//白色でHello, world!!と描画
     Sleep(0.5f);
-    DrawText("Hello, world!!", -12*14/2, 22, kColorYellow);
+    DrawText("Hello, world!!", -12*14/2, 22, kColorYellow);//黄色でHello, world!!と描画
     Sleep(0.5f);
-    DrawText("Hello, world!!", -12*14/2, 0, kColorGreen);
+    DrawText("Hello, world!!", -12*14/2, 0, kColorGreen);//緑色でHello, world!!と描画
     Sleep(0.5f);
-    DrawText("Hello, world!!", -12*14/2, -22, kColorBlue);
+    DrawText("Hello, world!!", -12*14/2, -22, kColorBlue);//青色でHello, world!!と描画
     Sleep(0.5f);
-    DrawText("Hello, world!!", -12*14/2, -44, kColorRed);
+    DrawText("Hello, world!!", -12*14/2, -44, kColorRed);//赤色でHello, world!!と描画
     Sleep(0.5f);
-    DrawText("Hello, world!!", -12*14/2, -66, kColorGray);
+    DrawText("Hello, world!!", -12*14/2, -66, kColorGray);//灰色でHello, world!!と描画
 }
 
